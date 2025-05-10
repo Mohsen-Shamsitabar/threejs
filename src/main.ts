b64b83e519b9db1b85933580bd4925033eadd45a
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-05-10 21:10:13 +0330

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,15 @@
 import "normalize.css";
 import WebGL from "three/examples/jsm/capabilities/WebGL.js";
 import "./style.css";
-import {
-  axesHelper,
-  hoveringCubeScene,
-  renderingLines,
-  renderingMultipleLines,
-  renderingText,
-} from "./tutorials/index.ts";
+import { hoveringCubeScene } from "./tutorials/index.ts";
 
 const root = document.getElementById("root");
 
 if (!root) throw new Error("No root element detected!");
 
-const scenes = {
-  HOVERING_CUBE: hoveringCubeScene(),
-  RENDERING_TEXT: renderingText(),
-  RENDERING_LINES: renderingLines(),
-  AXES_HELPER: axesHelper(),
-  RENDERING_MULTIPLE_LINES: renderingMultipleLines(),
-};
-
 if (WebGL.isWebGL2Available()) {
   // Initiate function or other initializations here
-  root.appendChild(scenes.RENDERING_MULTIPLE_LINES);
+  root.appendChild(hoveringCubeScene());
 } else {
   const warning = WebGL.getWebGL2ErrorMessage();
 
